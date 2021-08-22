c3f5cd655eae07b35681478b37ccbd112cb23a7b
raythompsonwebdev <ray_thomp@hushmail.com>
2021-08-22 12:48:25 +0100

    3 files modified

--- script.js
+++ script.js
@@ -13,3 +13,29 @@
 // Bonus - Add more elements to the section on the right
 // When the user clicks the buttons on the right, the content of the right hand side should update.
 // For example, you could update the right-hand-side to show information on three different artists and have their names in the buttons
+
+var buttonRed = document.getElementById('red-button');
+let greenButton = document.getElementById('green-button');
+let blueButton = document.getElementById('blue-button');
+
+let allButton = document.getElementsByTagName('button');
+console.log(allButton);
+let currentBgCol = getComputedStyle(document.body).getPropertyValue(
+  '--page-background'
+);
+
+for (let u = 0; u < allButton.length; u++) {
+  console.log(allButton[u]);
+  allButton[u].addEventListener('click', e => {
+    e.preventDefault();
+    if (currentBgCol !== allButton[u].textContent) {
+      allButton[u].classList.add("active");
+      document.body.style.setProperty(
+        '--page-background',
+        `${allButton[u].textContent}`
+      );     
+    } 
+  });
+
+
+}

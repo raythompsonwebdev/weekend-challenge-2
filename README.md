42cc8ad9061b02fce265ecf5db23deeda7a57124
raythompsonwebdev <ray_thomp@hushmail.com>
2021-08-22 11:26:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# weekend-challenge-2-eqnsjb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/weekend-challenge-2-eqnsjb)
---
+++ script.js
@@ -0,0 +1,15 @@
+// Your challenge is to update this JavaScript file so that when a user clicks the buttons on the left, the colour of the background on the right hand side changes.
+
+// Step One - Programme the Red button
+
+// Step Two - Add code for the Green and Blue buttons
+
+// Step Three - Have a look at your solution, could it be refactored to reduce the amount of repeated code?
+
+// Step Four - Add the active class to whichever button was just clicked, remove it from the previous buttons
+
+// Step Five - Add a reset function so that if a button was active and is clicked again, the page resets to a white background
+
+// Bonus - Add more elements to the section on the right
+// When the user clicks the buttons on the right, the content of the right hand side should update.
+// For example, you could update the right-hand-side to show information on three different artists and have their names in the buttons

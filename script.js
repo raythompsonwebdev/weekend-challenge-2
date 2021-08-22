4af91f9ee8bb2035394eb201f23fdeaa7d1b16de
raythompsonwebdev <ray_thomp@hushmail.com>
2021-08-22 21:19:48 +0100

    add comments

--- script.js
+++ script.js
@@ -14,42 +14,50 @@
 // When the user clicks the buttons on the right, the content of the right hand side should update.
 // For example, you could update the right-hand-side to show information on three different artists and have their names in the buttons
 
-// var buttonRed = document.getElementById('red-button');
-// let greenButton = document.getElementById('green-button');
-// let blueButton = document.getElementById('blue-button');
-
+// get all buttons
 let allButton = document.getElementsByTagName('button');
 
+//get color of current background
 let currentBgCol = getComputedStyle(document.body).getPropertyValue(
   '--page-background'
 );
 
+//set link to active function
 function myFunction(e) {
+  //if current target button not equal to null
   if (document.querySelector('.active') !== null) {
+    //remove active from any other button with active class
     document.querySelector('.active').classList.remove('active');
   }
+  //add active class to current event target
   e.target.className = "active";
 }
 
+//loop over buttons collection
 for (let u = 0; u < allButton.length; u++) {
    
+  //add event listener to each button
   allButton[u].addEventListener('click', e => {
 
+    //prevent default
     e.preventDefault();
     
+    // if button color equals background colour and class name not active
     if (currentBgCol !== allButton[u].textContent && allButton[u].className != "active" ) {
-
+      //set background color to background color
       document.body.style.setProperty(
         '--page-background',
         `${allButton[u].textContent}`
       );     
     }else{
+      //else remove active class and set background color to default white
       allButton[u].classList.remove('active')
       document.body.style.setProperty(
         '--page-background',
         `${currentBgCol}`
       ); 
-    }  
+    } 
+    // invoke active function passing in event object as argument 
     myFunction(e)
     
   });  

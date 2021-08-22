133264721dd1a097d26a0a70ba58fba14a725d1a
raythompsonwebdev <ray_thomp@hushmail.com>
2021-08-22 21:11:00 +0100

    add function

--- script.js
+++ script.js
@@ -14,41 +14,43 @@
 // When the user clicks the buttons on the right, the content of the right hand side should update.
 // For example, you could update the right-hand-side to show information on three different artists and have their names in the buttons
 
-var buttonRed = document.getElementById('red-button');
-let greenButton = document.getElementById('green-button');
-let blueButton = document.getElementById('blue-button');
+// var buttonRed = document.getElementById('red-button');
+// let greenButton = document.getElementById('green-button');
+// let blueButton = document.getElementById('blue-button');
 
 let allButton = document.getElementsByTagName('button');
-console.log(allButton);
+
 let currentBgCol = getComputedStyle(document.body).getPropertyValue(
   '--page-background'
 );
 
-
-
-  for (let u = 0; u < allButton.length; u++) {
-    //console.log(allButton[u]);
-    allButton[u].addEventListener('click', e => {
-      e.preventDefault();
-
-      //console.log(e.target)
-      if (currentBgCol !== allButton[u].textContent && allButton[u].className != "active") {
-              console.log(e.target.previousElementSibling)
-        allButton[u].classList.add("active");
-  
-        document.body.style.setProperty(
-          '--page-background',
-          `${allButton[u].textContent}`
-        );     
-      }else{
-        allButton[u].classList.remove("active");
-        document.body.style.setProperty(
-          '--page-background',
-          `${currentBgCol}`
-        );   
-      }
-      
-      
-    });  
-
+function myFunction(e) {
+  if (document.querySelector('.active') !== null) {
+    document.querySelector('.active').classList.remove('active');
   }
+  e.target.className = "active";
+}
+
+for (let u = 0; u < allButton.length; u++) {
+   
+  allButton[u].addEventListener('click', e => {
+
+    e.preventDefault();
+    
+    if (currentBgCol !== allButton[u].textContent && allButton[u].className != "active" ) {
+
+      document.body.style.setProperty(
+        '--page-background',
+        `${allButton[u].textContent}`
+      );     
+    }else{
+      allButton[u].classList.remove('active')
+      document.body.style.setProperty(
+        '--page-background',
+        `${currentBgCol}`
+      ); 
+    }  
+    myFunction(e)
+    
+  });  
+}
